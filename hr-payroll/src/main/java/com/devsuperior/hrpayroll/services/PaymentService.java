f5b08e01870caef1b98b268c46006a880f60a3cf
DevOstium <ostiumdev@gmail.com>
2020-11-21 10:05:03 -0300

    Config Hystrix com teste de falha de servic fora do ar

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -2,31 +2,19 @@ package com.devsuperior.hrpayroll.services;
 
 import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String> uriVariable = new HashMap<String, String>();
-
-        // convert em string = ""+workerId
-        uriVariable.put("id", "" + workerId);
-
-        Worker worker = restTemplate.getForObject (workerHost + "/workers/{id}", Worker.class, uriVariable);
+        Worker worker = workerFeignClients.findById(workerId).getBody();
 
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repo;
 
@@ -25,7 +33,20 @@ public class WorkerResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Worker> findById(@PathVariable Long id) {
+    public ResponseEntity<Worker> findById(@PathVariable Long id) throws InterruptedException {
+
+        // Com isso o microservice deve retornar o getPaymentAlternative configurado no @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+
+        // Para simulara um error de timeout com 5 segundos
+        // Por padrão o tempo do Ribbon é 1 segundo
+        // O Ribbon nem era o tempo de 5 segundos, assim que o seu tempo padrão é excedido ele já retorna o getPaymentAlternative
+        Thread.sleep(5000L);
+
+        // Para simulara um erro qualquer
+        throw new RuntimeException("Criar uma excption aqui só para simulara um error");
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repo.findById(id).get();
         return ResponseEntity.ok(obj);
     }
