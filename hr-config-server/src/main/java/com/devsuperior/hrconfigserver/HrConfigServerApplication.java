eca99ea08da98e0d95507adeb2f6fdc4dbf2233c
DevOstium <ostiumdev@gmail.com>
2020-11-21 16:57:40 -0300

    Fim da fase 3

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,16 +1,29 @@
 package com.devsuperior.hrconfigserver;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner {
+
+	//private static Logger logger = org.slf4j.LoggerFactory.getLogger(HrConfigServerApplication.class);
+
+	//@Value("${spring.cloud.config.server.git.username}")
+	//private String userNameGitHub;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// logger.info("Variavel de Ambiente - USERNAME= " + userNameGitHub);
+		//System.out.println(System.getenv("GITHUB_USER"));
 	}
 
 }
